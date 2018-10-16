74349dc23504eaf9af6c101c1ef53f722c82de96
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-10-16 11:12:21 -0700

    Initial commit, base files

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// these are for production. Clean it up later.
+// app.use(express.static(path.join(__dirname, '/../client/dist/'))).use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist', 'index.jsx'));
+// });
+
+app.use(express.static(path.join(__dirname, '/../client/dist/'))).use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.jsx'));
+});
+
+app.listen(PORT, () => console.log(`EnF Random Character Generator listening on ${PORT}`));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${SRC_DIR}/index.html`,
+    }),
+  ],
+  resolve: { extensions: ['.js', '.jsx'] },
+};
