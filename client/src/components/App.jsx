675866e3ea2bd58eb7c4f9f38855b48876e9032b
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-10-17 15:43:24 -0700

    Add initial function to simulate dice rolls

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  static rollDice(numberOfDice, sides) {
+    let rolls = [];
+    for (let i = 0; i < numberOfDice; i += 1) {
+      rolls[i] = 1 + Math.floor(Math.random() * sides);
+    }
+    return rolls;
+  }
+
   constructor(props) {
     super(props);
   }
