96b54eee55cebb043b388bf217a91528b3a252b2
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-10-18 13:50:43 -0700

    Sort die roll output and begin race choosing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  // returns an array of individual rolls
+  // returns an array of individual rolls, sorted low to high
   static rollDice(numberOfDice, sides) {
     let rolls = [];
     for (let i = 0; i < numberOfDice; i += 1) {
       rolls[i] = 1 + Math.floor(Math.random() * sides);
     }
+    rolls.sort((a, b) => a - b);
     return rolls;
+  }
+
+  static chooseRace(originalsOnly) {
+    let races = {
+      0: 'Arkhan',
+      1: 'Equirion',
+      2: 'Fibblan',
+      3: 'Human',
+      4: 'Iquoran',
+      5: 'Khibblan',
+    };
   }
 
   constructor(props) {
