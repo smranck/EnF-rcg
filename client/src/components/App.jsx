8556029dbd739e2ad9a55ce15f60a0ce4e264d01
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-10-28 09:26:24 -0700

    Add necessary fields to state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -308,9 +308,35 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      nativePreference: false,
+      originalClasses: false,
+      maxLevel: 20,
+      level: 1,
+      race: ['Race Not Assigned', 'Race Not Assigned'],
+      raceObject: {}, // this will be gotten eventually, won't need race above
+      class: '',
+      skills: {},
+      skillsModifier: 0,
+      qualities: [],
+      attributes: [],
+      str: 10,
+      spr: 10,
+      vit: 10,
+      dex: 10,
+      agi: 10,
+    };
   }
 
   render() {
-    return <div>Initial Component</div>;
+    return (
+      <div>
+        <div>Lorum Ipsum</div>
+        <div>
+          This will be a random character Generator for the game found at
+          https://sinowl.net/ebbandflow/
+        </div>
+      </div>
+    );
   }
 }
