90e0be7a8a48220055199842c15d8bccb74632ae
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-11-09 13:04:36 -0800

    Move character stats to different table

--- client/src/components/Character.jsx
+++ client/src/components/Character.jsx
@@ -28,124 +28,127 @@ export default class Character extends React.Component {
 
     return (
       <div className="character-container">
-        <table className="character-display">
-          <thead>
-            <tr>
-              <th>Property</th>
-              <th>Your Randomly Generated Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Level</td>
-              <td>{level}</td>
-            </tr>
-            <tr>
-              <td>Race</td>
-              <td>{race}</td>
-            </tr>
-            <tr>
-              <td>Special Trait</td>
-              <td>{raceTrait[0]}</td>
-            </tr>
-            <tr>
-              <td>Race Trait</td>
-              <td>{raceTrait[1] || traits[0]}</td>
-            </tr>
-            <tr>
-              <td>Class</td>
-              <td>{characterClass}</td>
-            </tr>
-            <tr>
-              <td>Class Path</td>
-              <td>{classPath}</td>
-            </tr>
-            <tr>
-              <td>Class Path Stage</td>
-              <td>{Math.floor(level / 2)}</td>
-            </tr>
-            <tr>
-              <td>Native Class Bonus</td>
-              <td>{nativeClassBonus || 'None'}</td>
-            </tr>
-            <tr>
-              <td>Strength</td>
-              <td>{str}</td>
-            </tr>
-            <tr>
-              <td>Spirit</td>
-              <td>{spr}</td>
-            </tr>
-            <tr>
-              <td>Vitality</td>
-              <td>{vit}</td>
-            </tr>
-            <tr>
-              <td>Dexterity</td>
-              <td>{dex}</td>
-            </tr>
-            <tr>
-              <td>Agility</td>
-              <td>{agi}</td>
-            </tr>
-            <tr>
-              <td>Attribute Points to Distribute:</td>
-              <td>{Math.floor(level / 5)}</td>
-            </tr>
-            <tr>
-              <td>Observation</td>
-              <td>{obs}</td>
-            </tr>
-            <tr>
-              <td>Charisma</td>
-              <td>{char}</td>
-            </tr>
-            <tr>
-              <td>Wisdom</td>
-              <td>{wis}</td>
-            </tr>
-            <tr>
-              <td>Quality Points to Distribute</td>
-              <td>{Math.floor(level / 5)}</td>
-            </tr>
-            <tr>
-              <td>Profession</td>
-              <td>{professions[0]}</td>
-            </tr>
-            <tr>
-              <td>Personality Trait</td>
-              <td>{personalityTrait}</td>
-            </tr>
-            <tr>
-              <td>Personality Forte</td>
-              <td>{firstForte}</td>
-            </tr>
-            <tr>
-              <td>Personality Forte</td>
-              <td>{secondForte}</td>
-            </tr>
-            <tr>
-              <td>Personality Flaw</td>
-              <td>{flaw}</td>
-            </tr>
-            {race === 'Valkyr Aspect' ? (
+        <div className="table-container">
+          <table className="character-stats">
+            <thead>
               <tr>
-                <td>Valkyr Aspect Special Profession</td>
-                <td>{professions[1]}</td>
+                <th colSpan="4"> Character Stats</th>
               </tr>
-            ) : null}
-            {traits.map(trait => (
-              <tr key={trait}>
-                <td>Trait</td>
-                <td>{trait}</td>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Strength</td>
+                <td>{str}</td>
+                <td>Observation</td>
+                <td>{obs}</td>
               </tr>
-            ))}
-            <tr>
-              <td>Total Characters Generated</td>
-              <td>{totalCharacters}</td>
-            </tr>
-          </tbody>
-        </table>
+              <tr>
+                <td>Spirit</td>
+                <td>{spr}</td>
+                <td>Charisma</td>
+                <td>{char}</td>
+              </tr>
+              <tr>
+                <td>Vitality</td>
+                <td>{vit}</td>
+                <td>Wisdom</td>
+                <td>{wis}</td>
+              </tr>
+              <tr>
+                <td>Agility</td>
+                <td>{agi}</td>
+                <td>Dexterity</td>
+                <td>{dex}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <table className="character-details">
+            <thead>
+              <tr>
+                <th colSpan="2">Character Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Level</td>
+                <td>{level}</td>
+              </tr>
+              <tr>
+                <td>Race</td>
+                <td>{race}</td>
+              </tr>
+              <tr>
+                <td>Special Trait</td>
+                <td>{raceTrait[0]}</td>
+              </tr>
+              <tr>
+                <td>Race Trait</td>
+                <td>{raceTrait[1] || traits[0]}</td>
+              </tr>
+              <tr>
+                <td>Class</td>
+                <td>{characterClass}</td>
+              </tr>
+              <tr>
+                <td>Class Path</td>
+                <td>{classPath}</td>
+              </tr>
+              <tr>
+                <td>Class Path Stage</td>
+                <td>{Math.floor(level / 2)}</td>
+              </tr>
+              <tr>
+                <td>Native Class Bonus</td>
+                <td>{nativeClassBonus || 'None'}</td>
+              </tr>
+              <tr>
+                <td>Attribute Points to Distribute:</td>
+                <td>{Math.floor(level / 5)}</td>
+              </tr>
+              <tr>
+                <td>Quality Points to Distribute</td>
+                <td>{Math.floor(level / 5)}</td>
+              </tr>
+              <tr>
+                <td>Profession</td>
+                <td>{professions[0]}</td>
+              </tr>
+              <tr>
+                <td>Personality Trait</td>
+                <td>{personalityTrait}</td>
+              </tr>
+              <tr>
+                <td>Personality Forte</td>
+                <td>{firstForte}</td>
+              </tr>
+              <tr>
+                <td>Personality Forte</td>
+                <td>{secondForte}</td>
+              </tr>
+              <tr>
+                <td>Personality Flaw</td>
+                <td>{flaw}</td>
+              </tr>
+              {race === 'Valkyr Aspect' ? (
+                <tr>
+                  <td>Valkyr Aspect Special Profession</td>
+                  <td>{professions[1]}</td>
+                </tr>
+              ) : null}
+              {traits.map(trait => (
+                <tr key={trait}>
+                  <td>Trait</td>
+                  <td>{trait}</td>
+                </tr>
+              ))}
+              <tr>
+                <td>Total Characters Generated</td>
+                <td>{totalCharacters}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
