fe1a46ebfa1ef160e90878d6b9e08a4ac6b9f8ba
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-11-09 17:56:04 -0800

    Add component to hold settings options

---
+++ client/src/components/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Character extends React.Component {
+  render() {
+    return (
+      <div className="settings">
+        <span>COMPONENT</span>
+      </div>
+    );
+  }
+}
