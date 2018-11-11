1119c4c77a0b27b802c58d38f3df761f1dfd6556
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-11-11 00:33:18 -0800

    Create initial 2 settings and incorporate function to handle changes

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -3,9 +3,31 @@ import React from 'react';
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Character extends React.Component {
   render() {
+    const { defaultStats, nativeRace, handleInputChange } = this.props;
     return (
       <div className="settings">
-        <span>COMPONENT</span>
+        <span>Settings</span>
+        <form>
+          <label htmlFor="defaultStatsCheckbox">
+            Default Stats?
+            <input
+              name="defaultStats"
+              type="checkbox"
+              checked={defaultStats}
+              onChange={e => handleInputChange(e)}
+            />
+          </label>
+          <br />
+          <label htmlFor="nativeRaceCheckbox">
+            Native Classes Only?
+            <input
+              name="nativeRace"
+              type="checkbox"
+              checked={nativeRace}
+              onChange={e => handleInputChange(e)}
+            />
+          </label>
+        </form>
       </div>
     );
   }
