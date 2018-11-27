b8000a53f7414ea4a6ee83edc9c1f40a82dc9825
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-11-26 22:11:02 -0800

    Remove buffer spans for more efficient spacing

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -25,7 +25,6 @@ export default class Character extends React.Component {
         <form>
           <label>
             Level
-            <span className="buffer" />
             <select
               value={desiredLevel || 'Any'}
               name="desiredLevel"
@@ -57,7 +56,6 @@ export default class Character extends React.Component {
           <br />
           <label>
             Race
-            <span className="buffer" />
             <select
               value={desiredRace || 'Any'}
               name="desiredRace"
@@ -84,7 +82,6 @@ export default class Character extends React.Component {
           <br />
           <label>
             Class
-            <span className="buffer" />
             <select
               value={desiredClass || 'Any'}
               name="desiredClass"
@@ -148,7 +145,6 @@ export default class Character extends React.Component {
           <br />
           <label>
             Profession?
-            <span className="buffer" />
             <select
               value={professionLikelihood}
               name="professionLikelihood"
