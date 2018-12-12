d64bca86ed729c3c802e456b632917fee8b59a46
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-12-11 22:56:52 -0800

    Remove outdated commented code

--- server/index.js
+++ server/index.js
@@ -6,13 +6,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// these are for production. Clean it up later.
-// app.use(express.static(path.join(__dirname, '/../client/dist/'))).use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist', 'index.jsx'));
-// });
-
 app.use(express.static(path.join(__dirname, '/../client/dist/'))).use(bodyParser.json());
 
 app.get('/', (req, res) => {
